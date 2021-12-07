adffbc685574f7ded5bf2c193e605a51dc19725c
Arsen92m <arsen1992m@mail.ru>
2021-12-07 22:52:17 +0400

    ResizeObserver

---
+++ ResizeObserver.js
@@ -0,0 +1,26 @@
+const box = document.getElementById("box");
+const changeStyles = (elements, properties, values) =>
+    elements.forEach((element, index) => {
+        element.style[properties[index]] = values[index];
+    });
+
+const observer = new ResizeObserver((entries) => {
+     for (const entry of entries) {
+        const width = entry.contentRect.width;
+
+       if (width < 768) {
+           changeStyles(
+               [box, title, text, info],
+               ["filter", "fontSize", "fontSize", "opacity"],
+               ["invert(100%)", "1.5rem", "1rem", "0"]
+            );
+        } else {
+           changeStyles(
+                [box, title, text, info],
+               ["filter", "fontSize", "fontSize", "opacity"],
+               ["invert(0%)",  "2rem", "1.25rem", "1"]
+            );
+        }
+    }
+});
+observer.observe(box);
